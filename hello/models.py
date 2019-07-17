432e26a28d0878f3781a9d266e0856ad3786bc18
yui728 <yui.nishimura.0728@gmail.com>
2019-07-17 16:51:46 +0900

    モデル定義を追加

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Customer(models.Model):
+    # id = AutoField(primary_key = True) # 自動的に追加されるので定義不要
+    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    email_address = models.EmailField(max_length=255, unique=True)
+    memo = models.TextField(null=True)
+
 class Hello(models.Model):
     your_name = models.CharField(max_length=10)
 
